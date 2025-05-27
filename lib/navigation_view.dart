7b121719cdcbf39b0e3b3c0f7933db2603bc1105
Oleksandr Parfionov <parfyonator@gmail.com>
2025-05-27 13:24:22 +0300

    Start from the left to right

--- lib/navigation_view.dart
+++ lib/navigation_view.dart
@@ -47,7 +47,7 @@ class _NavigationView extends State<NavigationView>{
   @override
   void initState() {
      if(widget.durationAnimation != null) durationAnimation = widget.durationAnimation!;
-    _currentPage =  0;
+    _currentPage =  widget.items.length - 1;
     super.initState();
   }
 
